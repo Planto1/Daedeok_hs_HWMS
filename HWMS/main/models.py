89a51f6046003fb72bdc3c22d4b91f64d12ce009
Planto <ssangho1239@gmail.com>
2025-12-16 03:04:57 +0900

    UPDATE

--- HWMS/main/models.py
+++ HWMS/main/models.py
@@ -1,4 +1,75 @@
 from django.db import models
-from api import Firms
+import requests
+import pandas as pd
+from datetime import datetime
 
 # Create your models here.
+
+MAP_KEY = '5872ff30914a691ad9aa8eaf6e5410a7'
+
+area_url = 'https://firms.modaps.eosdis.nasa.gov/api/area/csv/' + MAP_KEY + '/VIIRS_NOAA20_NRT/125,33,130,38.5/10/2025-11-21'
+df_area = pd.read_csv(area_url)
+
+
+
+class FireDetection(models.Model):
+    latitude = models.FloatField()
+    longitude = models.FloatField()
+    bright_ti4 = models.FloatField()
+    scan = models.FloatField()
+    track = models.FloatField()
+    acq_date = models.DateField()
+    acq_time = models.CharField(max_length=4)
+    satellite = models.CharField(max_length=10)
+    instrument = models.CharField(max_length=10)
+    confidence = models.CharField(max_length=1)
+    version = models.CharField(max_length=10)
+    bright_ti5 = models.FloatField()
+    frp = models.FloatField()
+    daynight = models.CharField(max_length=1)
+    
+    class Meta:
+        db_table = 'fire_detection'
+    
+    def __str__(self):
+        return f"Fire at ({self.latitude}, {self.longitude}) on {self.acq_date}"
+    
+    @classmethod
+    def save_from_dataframe(cls, df):
+        FireDetection.save_from_dataframe(df_area)
+        fire_list = []
+        
+        for idx, row in df.iterrows():
+            # 날짜 변환
+            if isinstance(row['acq_date'], str):
+                acq_date = datetime.strptime(row['acq_date'], '%Y-%m-%d').date()
+            else:
+                acq_date = row['acq_date']
+            
+            # 시간을 4자리 문자열로
+            acq_time = str(row['acq_time']).zfill(4)
+            
+            fire_obj = cls(
+                latitude=row['latitude'],
+                longitude=row['longitude'],
+                bright_ti4=row['bright_ti4'],
+                scan=row['scan'],
+                track=row['track'],
+                acq_date=acq_date,
+                acq_time=acq_time,
+                satellite=row['satellite'],
+                instrument=row['instrument'],
+                confidence=row['confidence'],
+                version=row['version'],
+                bright_ti5=row['bright_ti5'],
+                frp=row['frp'],
+                daynight=row['daynight']
+            )
+            
+            fire_list.append(fire_obj)
+        
+        # 한번에 저장
+        cls.objects.bulk_create(fire_list, batch_size=1000)
+        
+        print(f"{len(fire_list)}개 데이터 저장 완료!")
+        return len(fire_list)
--- HWMS/main/views.py
+++ HWMS/main/views.py
@@ -1,5 +1,40 @@
 from django.shortcuts import render
+from django.http import JsonResponse
+from .models import FireDetection
+
 
 # Create your views here.
 def index(request):
     return render(request,'main/index.html')
+
+'''
+def fire_map_view(request):
+    """지도 페이지"""
+    return render(request, 'fire_map.html')
+'''
+
+def fire_data_api(request):
+    """화재 데이터를 JSON으로 반환"""
+    fires = FireDetection.objects.all().values(
+        'latitude', 
+        'longitude', 
+        'frp', 
+        'bright_ti4',
+        'acq_date',
+        'acq_time',
+        'satellite'
+    )
+    
+    fire_list = []
+    for fire in fires:
+        fire_list.append({
+            'latitude': fire['latitude'],
+            'longitude': fire['longitude'],
+            'frp': fire['frp'],
+            'bright_ti4': fire['bright_ti4'],
+            'acq_date': str(fire['acq_date']),
+            'acq_time': fire['acq_time'],
+            'satellite': fire['satellite']
+        })
+    
+    return JsonResponse(fire_list, safe=False)
